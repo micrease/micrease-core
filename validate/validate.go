0039d3beffbe356818b61e8ce1bee567fe09e9f1
uther518 <uther518@gmail.com>
2022-04-08 14:54:29 +0800

    validator

--- validate/validate.go
+++ validate/validate.go
@@ -2,44 +2,43 @@ package validate
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/micrease/micrease-core/context"
 	"github.com/micrease/micrease-core/errs"
 	"reflect"
 )
 
 //如果绑定参数失败，则中断请求并抛出错误返回给客户端
-func BindWithPanic(ctx *gin.Context, obj interface{}) error {
+func BindWithPanic(ctx *context.Context, obj interface{}) error {
 	return bind(ctx, obj, true)
 }
 
 //如果绑定失败，则返回错误
-func Bind(ctx *gin.Context, obj interface{}) error {
+func Bind(ctx *context.Context, obj interface{}) error {
 	return bind(ctx, obj, false)
 }
 
-func bind(ctx *gin.Context, obj interface{}, isPanic bool) error {
+func bind(ctx *context.Context, obj interface{}, isPanic bool) error {
 	//绑定参数
-	err := ctx.Bind(obj)
+	err := ctx.GinCtx.Bind(obj)
 	if err != nil {
 		//解析错误信息
 		value := reflect.TypeOf(obj)
 		if validErrs, ok := err.(validator.ValidationErrors); ok {
 			for _, e := range validErrs {
 				if f, exist := value.Elem().FieldByName(e.Field()); exist {
+					var paramError error
 					if value, ok := f.Tag.Lookup("tips"); ok {
-						paramError := fmt.Errorf("%s", value)
-						if isPanic {
-							errs.PanicIfParamError(paramError)
-						}
-						return paramError
+						paramError = fmt.Errorf("%s", value)
 					} else {
-						paramError := fmt.Errorf("%s", e.Value())
-						if isPanic {
-							errs.PanicIfParamError(paramError)
-						}
-						return paramError
+						paramError = fmt.Errorf("%s", e.Value())
 					}
+
+					if isPanic {
+						er := errs.Err(errs.StatusParamError, paramError.Error(), paramError)
+						panic(er)
+					}
+					return paramError
 				}
 			}
 		}
